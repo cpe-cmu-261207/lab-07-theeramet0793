9118930750efd3b980d95ec4752ff9c2ecb6f752
theeramet0793 <theerametmetha@gmail.com>
2021-02-02 10:51:16 +0700

    finish index

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,18 @@ import axios from 'axios'
 const baseURL = 'https://dummyapi.io/data/api'
 const Home = () => {
   return (
+    <>
       <h1>This is home page</h1>
+      <link href="post">
+        <button>goto post page</button>
+      </link>
+      <link href="/calGPA">
+        <button>Calculate GPA</button>
+      </link>
+      <link href="/port">
+        <button>Portfolio</button>
+      </link>
+      </>
   )
 }
 
